a00afc33fe9da47e8bd8c0edbb91434e8585901c
Vaikesh K P <vaikesh4leo@gmail.com>
2015-11-14 16:48:07 +0530

    Dynamic Control
    Create a text view and edit text dynamically when a button is clicked.

---
+++ DynamicControl/DynamicControl/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DynamicControl
+{
+	[Activity (Label = "DynamicControl", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 0;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+
+			ScrollView _scrl = new ScrollView (this);
+			LinearLayout _linear = new LinearLayout (this);
+			_linear.Orientation = Orientation.Vertical;
+			_scrl.AddView (_linear);
+
+			Button _addBtn = new Button (this);
+			_addBtn.Text = "Click to add TextViiews and EditTexts";
+			_linear.AddView (_addBtn);
+
+			_addBtn.Click += (sender, e) => {
+				
+				// TODO Auto-generated method stub
+				count++;
+
+				TextView _text = new TextView (ApplicationContext);
+				_text.Text = "Number : " + count.ToString();
+				_linear.AddView(_text);
+
+				EditText _edit = new EditText(ApplicationContext);
+				_edit.Text = " Edit Text : " + count.ToString();
+				_linear.AddView(_edit);
+
+			};
+
+			this.SetContentView (_scrl);
+		}
+	}
+}
